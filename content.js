c6de7517e87ccb92e214b819946bc5fecc73b668
deliciouswaffle <131652318+deliciouswaffle@users.noreply.github.com>
2023-04-25 15:59:18 +0900

    Add files via upload

---
+++ content.js
@@ -0,0 +1,51 @@
+window.onload=click()
+
+function click() {
+    const button=document.querySelectorAll(".css-1adjw8a")[0]
+    button.addEventListener('click', function() {
+        change();
+        assa();
+    });
+};
+
+function change() {
+    var a = document.querySelector("#Write");
+  
+    var $ = (text) => {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      ctx.font = "16px NanumSquareWebFont";
+  
+      if (text.includes("\n")) {
+        const maxWidth = 619;
+        const lines = text.split("\n");
+        let result = "";
+  
+        lines.forEach((line) => {
+          if (!line.trim()) {
+            line = "ㅤ";
+          }
+  
+          const width = ctx.measureText(line).width;
+          const spacesNeeded = Math.ceil(
+            (maxWidth - width) / (ctx.measureText("　").width - ctx.measureText(" ").width)
+          );
+          result += line + "　".repeat(spacesNeeded);
+        });
+  
+        return result;
+      }
+  
+      return text;
+    };
+  
+    resultText = $(a.value.replace(/\$/g, "\n")).trim();
+    //alert('글을 등록하기 전 꼭 아무데나 띄어쓰기를 한번 해주세요.')
+  }
+
+function assa() {
+    chrome.tabs.executeScript(
+      null,
+      { code: `document.querySelector('#Write').value='${resultText}'` }
+    );
+}
